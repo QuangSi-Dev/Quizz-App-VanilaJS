ee0034595ee04dc6ea1c880384092d14380b3db3
Quang Si <tranquangsidev@gmail.com>
2021-02-03 01:12:30 +0700

    'quiz-app'

---
+++ index.js
@@ -0,0 +1,148 @@
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+
+let shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener("click", startGame);
+
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+
+  setNextQuestion();
+});
+
+function startGame() {
+  startButton.classList.add("hide");
+
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+
+  currentQuestionIndex = 0;
+
+  questionContainerElement.classList.remove("hide");
+
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  resetState();
+
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+
+    button.innerText = answer.text;
+
+    button.classList.add("btn");
+
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+
+    button.addEventListener("click", selectAnswer);
+
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  clearStatusClass(document.body);
+
+  nextButton.classList.add("hide");
+
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+
+  const correct = selectedButton.dataset.correct;
+
+  setStatusClass(document.body, correct);
+
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Restart";
+
+    startButton.classList.remove("hide");
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+
+  element.classList.remove("wrong");
+}
+
+const questions = [
+  {
+    question: "Trong các cầu thủ sau,cầu thủ nào gốc Việt nào?",
+
+    answers: [
+      { text: "Ronaldo", correct: false },
+
+      { text: "Messi", correct: false },
+
+      { text: "Neymar", correct: false },
+
+      { text: "Công Phượng", correct: true },
+    ],
+  },
+  {
+    question: "What is 2 + 2?",
+
+    answers: [
+      { text: "4", correct: true },
+
+      { text: "22", correct: false },
+    ],
+  },
+
+  {
+    question: "Học lập trình vui không?",
+
+    answers: [
+      { text: "Không", correct: false },
+
+      { text: "Vui!!!", correct: true },
+
+      { text: "Hơi hơi", correct: false },
+
+      { text: "Quay đầu là bờ", correct: false },
+    ],
+  },
+
+  {
+    question: "What is 4 * 2?",
+
+    answers: [
+      { text: "6", correct: false },
+
+      { text: "8", correct: true },
+    ],
+  },
+];
